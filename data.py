6e44fb2e0eb74fb673b59b801963d3bb079473b5
SoanKim <39689481+SoanKim@users.noreply.github.com>
2023-02-12 22:36:00 +0900

    ok

--- data.py
+++ data.py
@@ -1,36 +1,39 @@
 import numpy as np
 from collections import Counter
 
-n_prb = 1
-def one_hot(a, num_classes):
-    return np.squeeze(np.eye(num_classes)[a.reshape(-1)])
+n_prb = 1000
 
-def answer_gen(n_prb):
-    answer_li = []
-    for prb in range(n_prb):
-        temp_li = []
-        answer = np.random.choice(list(range(5)), 3, replace=False)
-        for num in answer:
-            temp_li.append(num)
-        answer_li.append(tuple(sorted(temp_li)))
-    return answer_li
+problem_mat = np.zeros((5, 4, 3))
 
-answer = answer_gen(n_prb)
-print(answer)
 
-total_mat = np.zeros((5 *4 *3))
+problem_mat = np.zeros((n_prb, 5, 4, 3))
+answer_attribute = []
+non_answer_attribute = []
 
-# #def problem_gen(n_prb):
-# total_prb = np.zeros((n_prb, 5, 4, 3))
-# for prb in range(n_prb):
-#     one_prb_li = []
-#     for card in range(5):
-#         one_att_li = []
-#         for attribute in range(4):
-#             value = np.random.choice(list(range(3)))
-#             print("####", prb, card, attribute, value)
-#             value = one_hot(value, 3)
-#             total_prb[prb, card, attribute] = value
-#
-# print(total_prb)
+for random_value in range(3000):
 
+    random_attribute = np.random.choice(list(range(3)), 3, replace=True)
+
+    if np.sum(random_attribute) % 3 == 0:
+        answer_attribute.append(random_attribute)
+    else:
+        non_answer_attribute.append(random_attribute)
+
+
+answer_li = []
+for prb in range(n_prb):
+
+    triplet = sorted(np.random.choice(list(range(5)), 3, replace=False))
+    three_answers = sorted([list(range(5))[i] for i in triplet])
+    answer_li.append(tuple(three_answers))
+    # two_distractors = sorted([list(range(5))[item] for item in list(range(5)) if item not in triplet])
+    # print("answers", three_answers)
+    for card in range(5):
+        for attribute in range(4):
+            for value in range(3):
+                if card in three_answers:
+                    problem_mat[prb, card, attribute] = answer_attribute[np.random.choice(list(range(len(answer_attribute))))]
+                elif card not in three_answers:
+                    problem_mat[prb, card, attribute] = non_answer_attribute[np.random.choice(list(range(len(non_answer_attribute))))]
+
+print(Counter(answer_li))
